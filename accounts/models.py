4c48b544b33b999f487f7dbd84a40935dadb448f
Adna-19 <adilsaleem1398@gmail.com>
2021-05-01 05:30:33 -0700

    Common fields image, gender placed in Abstract User

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,15 @@ class User(AbstractUser):
     ('S', 'STUDENT'),
     ('T', 'TEACHER'),
   )
+
+  GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+  )
+
   user_role = models.CharField(max_length=15, choices=USER_CHOICES)
+  gender    = models.CharField(max_length=10, choices=GENDER_CHOICES, default='M')
+  profile_image  = models.ImageField(upload_to='profile_images/teachers_profile_images/', null=True, blank=True, default='utility_images/default_image.png')
 
   def save(self, *args, **kwargs):
     created = self.id is None
@@ -22,34 +30,21 @@ class User(AbstractUser):
     return f"{self.first_name} {self.last_name}"
 
 class TeacherProfile(models.Model):
-  GENDER_CHOICES = (
-    ('M', 'Male'),
-    ('F', 'Female'),
-  )
   user           = models.OneToOneField(User, related_name='teacher_profile', on_delete=models.CASCADE)
-  gender         = models.CharField(max_length=10, choices=GENDER_CHOICES, default='M')
-  profile_image  = models.ImageField(upload_to='profile_images/teachers_profile_images/')
-  place_of_birth = models.CharField(max_length=30)
-  about             = models.TextField()
-  facebook_profile  = models.URLField()
-  twitter_profile   = models.URLField()
-  github_profile    = models.URLField()
-  instagram_profile = models.URLField()
+  place_of_birth = models.CharField(max_length=30, null=True, blank=True)
+  about             = models.TextField(null=True, blank=True)
+  facebook_profile  = models.URLField(null=True, blank=True)
+  twitter_profile   = models.URLField(null=True, blank=True)
+  github_profile    = models.URLField(null=True, blank=True)
+  instagram_profile = models.URLField(null=True, blank=True)
 
   def __str__(self):
     return f"{self.user.full_name}"
 
-
 class StudentProfile(models.Model):
-  GENDER_CHOICES = (
-    ('M', 'Male'),
-    ('F', 'Female'),
-  )
   user          = models.OneToOneField(User, related_name='student_profile', on_delete=models.CASCADE)
-  profile_image = models.ImageField(upload_to='profile_images/students_profile_images/')
-  gender        = models.CharField(max_length=10, choices=GENDER_CHOICES)
   interests     = models.ManyToManyField(Subject, blank=True)
-  bio           = models.TextField()
+  bio           = models.TextField(null=True, blank=True)
 
   def __str__(self):
     return f"{self.user.full_name}"
